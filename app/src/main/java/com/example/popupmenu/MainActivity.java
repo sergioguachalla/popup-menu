0d5f8557e0c67796f7aff1fa6fe92cedcd71edfb
Sergio Guachalla <sergioguachalla0@gmail.com>
2023-08-23 08:15:24 -0400

    pop up menus added

---
+++ app/src/main/java/com/example/popupmenu/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.popupmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonPopupMenu, buttonPopupMenu2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonPopupMenu = findViewById(R.id.btPopupMenu);
+        buttonPopupMenu2 = findViewById(R.id.btPopupMenu2);
+
+
+        buttonPopupMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this,buttonPopupMenu);
+                popupMenu.getMenuInflater().inflate(R.menu.layout_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        if(menuItem.getItemId() == R.id.popupMenuOne){
+                            Toast.makeText(MainActivity.this,"Elegido uno", Toast.LENGTH_LONG);
+
+                        }
+
+                        if(menuItem.getItemId() == R.id.popupMenuTwo){
+                            Toast.makeText(MainActivity.this,"Elegido dos", Toast.LENGTH_LONG);
+
+                        }
+
+                        if(menuItem.getItemId() == R.id.popupMenuThree){
+                            Toast.makeText(MainActivity.this,"Elegido tres", Toast.LENGTH_LONG);
+
+                        }
+
+                        if(menuItem.getItemId() == R.id.popupMenuFour){
+                            Toast.makeText(MainActivity.this,"Elegido cuatro", Toast.LENGTH_LONG);
+
+                        }
+
+                        return true;
+                    }
+                });
+                popupMenu.show();
+
+            }
+        });
+
+        buttonPopupMenu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this,buttonPopupMenu2);
+                popupMenu.getMenuInflater().inflate(R.menu.layout_second, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        if(menuItem.getItemId() == R.id.Menu2Item1){
+                            Toast.makeText(MainActivity.this,"Elegido azul", Toast.LENGTH_LONG).show();
+
+                        }
+
+                        if(menuItem.getItemId() == R.id.Menu2Item2){
+                            Toast.makeText(MainActivity.this,"Elegido rojo", Toast.LENGTH_LONG).show();
+
+                        }
+
+                        if(menuItem.getItemId() == R.id.Menu2Item3){
+                            Toast.makeText(MainActivity.this,"Elegido amarillo", Toast.LENGTH_LONG).show();
+
+                        }
+
+                        if(menuItem.getItemId() == R.id.Menu2Item4){
+                            Toast.makeText(MainActivity.this,"Elegido verde", Toast.LENGTH_LONG).show();
+
+                        }
+
+                        if(menuItem.getItemId() == R.id.Menu2Item5){
+                            Toast.makeText(MainActivity.this,"Elegido celeste", Toast.LENGTH_LONG).show();
+
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+
+            }
+        });
+
+
+
+    }
+}
